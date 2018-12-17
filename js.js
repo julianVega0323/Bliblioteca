3c19ad49c6034deae91467bb3473ecc0079553b6
BECL-UFPS <45946456+BECL-UFPS@users.noreply.github.com>
2018-12-17 12:07:02 -0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,58 @@
+$('#link-todas').click(function(event) {
+	$('#contenedor').load('BasesBibliograficas.html');
+});
+$('#link-Emerald').click(function(event) {
+	$('#contenedor').load('TutorialEmerald.html');
+});
+$('#link-gestores').click(function(event) {
+	$('#contenedor').load('GestoresBibliograficos.html');
+});
+$('#link-mendeley').click(function(event) {
+	$('#contenedor').load('TutorialMendeley.html');
+});
+$('#link-Zotero').click(function(event) {
+	$('#contenedor').load('TutorialZotero.html');
+});
+$('#link-Citeulike').click(function(event) {
+	$('#contenedor').load('TutorialCiteulike.html');
+});
+$('#link-ACM').click(function(event) {
+	$('#contenedor').load('TutorialACM.html');
+});
+$('#link-Ebsco').click(function(event) {
+	$('#contenedor').load('TutorialEbsco.html');
+});
+$('#link-Embase').click(function(event) {
+	$('#contenedor').load('TutorialEmbase.html');
+});
+
+$('#link-Village').click(function(event) {
+	$('#contenedor').load('TutorialEngineeringVillage.html');
+});
+$('#link-Gale').click(function(event) {
+	$('#contenedor').load('TutorialGale.html');
+});
+$('#link-Legis').click(function(event) {
+	$('#contenedor').load('TutorialLegis.html');
+});
+$('#link-Micromedex').click(function(event) {
+	$('#contenedor').load('TutorialMicromedex.html');
+});
+$('#link-NNNConsult').click(function(event) {
+	$('#contenedor').load('TutorialNNNConsult.html');
+});
+$('#link-Ovid').click(function(event) {
+	$('#contenedor').load('TutorialOvid.html');
+});
+$('#link-Reaxys').click(function(event) {
+	$('#contenedor').load('TutorialReaxys.html');
+});
+$('#link-Scopus').click(function(event) {
+	$('#contenedor').load('TutorialScopus.html');
+});
+$('#link-Direct').click(function(event) {
+	$('#contenedor').load('TutorialDirect.html');
+});
+$('#link-Web').click(function(event) {
+	$('#contenedor').load('TutorialWeb.html');
+});
