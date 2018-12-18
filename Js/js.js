93284d3c8707195b5d26b89f2ba2755fefc9b465
GersonLazaro <gersonlazaro@gersonlazaro.com>
2018-12-17 23:44:53 -0500

    Organizacion de carpetas

---
+++ Js/js.js
@@ -0,0 +1,414 @@
+$('#link-Ebsco1').click(function(event) {
+	$('#contenedor').load('TutorialEbsco.html');
+});
+
+$('#link-Emerald1').click(function(event) {
+	$('#contenedor').load('TutorialEmerald.html');
+});
+
+$('#link-ScienceDirect1').click(function(event) {
+	$('#contenedor').load('TutorialScienceDirect.html');
+});
+
+$('#link-WebofScience1').click(function(event) {
+	$('#contenedor').load('TutorialWebofScience.html');
+});
+
+$('#link-Ebsco11').click(function(event) {
+	$('#contenedor').load('TutorialEbsco.html');
+});
+
+
+
+$('#link-Scopus2').click(function(event) {
+	$('#contenedor').load('TutorialScopus.html');
+});
+
+$('#link-ScienceDirect2').click(function(event) {
+	$('#contenedor').load('TutorialScienceDirect.html');
+});
+
+$('#link-WebofScience2').click(function(event) {
+	$('#contenedor').load('TutorialWebofScience.html');
+});
+
+$('#link-Ebsco2').click(function(event) {
+	$('#contenedor').load('TutorialEbsco.html');
+});
+
+$('#link-Ebsco22').click(function(event) {
+	$('#contenedor').load('TutorialEbsco.html');
+});
+
+$('#link-Micromedex2').click(function(event) {
+	$('#contenedor').load('TutorialMicromedex.html');
+});
+
+$('#link-Reaxys2').click(function(event) {
+	$('#contenedor').load('TutorialReaxys.html');
+});
+
+
+
+$('#link-Reaxys3').click(function(event) {
+	$('#contenedor').load('TutorialReaxys.html');
+});
+
+$('#link-Scopus3').click(function(event) {
+	$('#contenedor').load('TutorialScopus.html');
+});
+
+$('#link-ScienceDirect3').click(function(event) {
+	$('#contenedor').load('TutorialScienceDirect.html');
+});
+
+$('#link-WebofScience3').click(function(event) {
+	$('#contenedor').load('TutorialWebofScience.html');
+});
+
+$('#link-Ebsco3').click(function(event) {
+	$('#contenedor').load('TutorialEbsco.html');
+});
+
+$('#link-Ebsco33').click(function(event) {
+	$('#contenedor').load('TutorialEbsco.html');
+});
+
+$('#link-Micromedex3').click(function(event) {
+	$('#contenedor').load('TutorialMicromedex.html');
+});
+
+
+
+$('#link-Embase4').click(function(event) {
+	$('#contenedor').load('TutorialEmbase.html');
+});
+
+$('#link-Scopus4').click(function(event) {
+	$('#contenedor').load('TutorialScopus.html');
+});
+
+$('#link-ScienceDirect4').click(function(event) {
+	$('#contenedor').load('TutorialScienceDirect.html');
+});
+
+$('#link-WebofScience4').click(function(event) {
+	$('#contenedor').load('TutorialWebofScience.html');
+});
+
+$('#link-NNNConsult4').click(function(event) {
+	$('#contenedor').load('TutorialNNNConsult.html');
+});
+
+$('#link-Ebsco4').click(function(event) {
+	$('#contenedor').load('TutorialEbsco.html');
+});
+
+$('#link-Ebsco44').click(function(event) {
+	$('#contenedor').load('TutorialEbsco.html');
+});
+
+$('#link-Micromedex4').click(function(event) {
+	$('#contenedor').load('TutorialMicromedex.html');
+});
+
+
+
+$('#link-Ebsco5').click(function(event) {
+	$('#contenedor').load('TutorialEbsco.html');
+});
+
+$('#link-ScienceDirect5').click(function(event) {
+	$('#contenedor').load('TutorialScienceDirect.html');
+});
+
+$('#link-WebofScience5').click(function(event) {
+	$('#contenedor').load('TutorialWebofScience.html');
+});
+
+$('#link-Ebsco55').click(function(event) {
+	$('#contenedor').load('TutorialEbsco.html');
+});
+
+$('#link-Scopus5').click(function(event) {
+	$('#contenedor').load('TutorialScopus.html');
+});
+
+
+
+$('#link-Ebsco6').click(function(event) {
+	$('#contenedor').load('TutorialEbsco.html');
+});
+
+$('#link-Legis6').click(function(event) {
+	$('#contenedor').load('TutorialLegis.html');
+});
+
+$('#link-ScienceDirect6').click(function(event) {
+	$('#contenedor').load('TutorialScienceDirect.html');
+});
+
+$('#link-WebofScience6').click(function(event) {
+	$('#contenedor').load('TutorialWebofScience.html');
+});
+
+$('#link-Ebsco66').click(function(event) {
+	$('#contenedor').load('TutorialEbsco.html');
+});
+
+$('#link-Scopus6').click(function(event) {
+	$('#contenedor').load('TutorialScopus.html');
+});
+
+
+
+$('#link-EngineeringVillage7').click(function(event) {
+	$('#contenedor').load('TutorialEngineeringVillage.html');
+});
+
+$('#link-Ebsco7').click(function(event) {
+	$('#contenedor').load('TutorialEbsco.html');
+});
+
+$('#link-ScienceDirect7').click(function(event) {
+	$('#contenedor').load('TutorialScienceDirect.html');
+});
+
+$('#link-WebofScience7').click(function(event) {
+	$('#contenedor').load('TutorialWebofScience.html');
+});
+
+$('#link-Ebsco77').click(function(event) {
+	$('#contenedor').load('TutorialEbsco.html');
+});
+
+$('#link-Scopus7').click(function(event) {
+	$('#contenedor').load('TutorialScopus.html');
+});
+
+$('#link-ACM7').click(function(event) {
+	$('#contenedor').load('TutorialACM.html');
+});
+
+
+
+$('#link-Scopus8').click(function(event) {
+	$('#contenedor').load('TutorialScopus.html');
+});
+
+$('#link-Ebsco8').click(function(event) {
+	$('#contenedor').load('TutorialEbsco.html');
+});
+
+$('#link-ScienceDirect8').click(function(event) {
+	$('#contenedor').load('TutorialScienceDirect.html');
+});
+
+$('#link-Ebsco88').click(function(event) {
+	$('#contenedor').load('TutorialEbsco.html');
+});
+
+$('#link-WebofScience8').click(function(event) {
+	$('#contenedor').load('TutorialWebofScience.html');
+});
+
+
+$('#link-Ebsco10').click(function(event) {
+	$('#contenedor').load('TutorialEbsco.html');
+});
+
+$('#link-ScienceDirect10').click(function(event) {
+	$('#contenedor').load('TutorialScienceDirect.html');
+});
+
+$('#link-Ebsco101').click(function(event) {
+	$('#contenedor').load('TutorialEbsco.html');
+});
+
+$('#link-WebofScience10').click(function(event) {
+	$('#contenedor').load('TutorialWebofScience.html');
+});
+
+
+
+$('#link-Gale').click(function(event) {
+	$('#contenedor').load('TutorialGale.html');
+});
+$('#link-Ovid').click(function(event) {
+	$('#contenedor').load('TutorialOvid.html');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$('#link-gestores').click(function(event) {
+	$('#contenedor').load('GestoresBibliograficos.html');
+});
+
+$('#link-Mendeley').click(function(event) {
+	$('#contenedor').load('TutorialMendeley.html');
+});
+
+$('#link-Zotero').click(function(event) {
+	$('#contenedor').load('TutorialZotero.html');
+});
+
+$('#link-Citeulike').click(function(event) {
+	$('#contenedor').load('TutorialCiteulike.html');
+});
+
+
+
+$('#link-todas').click(function(event) {
+	$('#contenedor').load('BasesBibliograficas.html');
+});
+
+$('#link-Emerald').click(function(event) {
+	$('#contenedor').load('TutorialEmerald.html');
+});
+
+$('#link-ACM').click(function(event) {
+	$('#contenedor').load('TutorialACM.html');
+});
+
+$('#link-Ebsco').click(function(event) {
+	$('#contenedor').load('TutorialEbsco.html');
+});
+
+$('#link-Embase').click(function(event) {
+	$('#contenedor').load('TutorialEmbase.html');
+});
+
+$('#link-EngineeringVillage').click(function(event) {
+	$('#contenedor').load('TutorialEngineeringVillage.html');
+});
+
+$('#link-Gale').click(function(event) {
+	$('#contenedor').load('TutorialGale.html');
+});
+
+$('#link-Legis').click(function(event) {
+	$('#contenedor').load('TutorialLegis.html');
+});
+
+$('#link-Micromedex').click(function(event) {
+	$('#contenedor').load('TutorialMicromedex.html');
+});
+
+$('#link-NNNConsult').click(function(event) {
+	$('#contenedor').load('TutorialNNNConsult.html');
+});
+
+$('#link-Ovid').click(function(event) {
+	$('#contenedor').load('TutorialOvid.html');
+});
+
+$('#link-Reaxys').click(function(event) {
+	$('#contenedor').load('TutorialReaxys.html');
+});
+
+$('#link-Scopus').click(function(event) {
+	$('#contenedor').load('TutorialScopus.html');
+});
+
+$('#link-ScienceDirect').click(function(event) {
+	$('#contenedor').load('TutorialScienceDirect.html');
+});
+
+$('#link-WebofScience').click(function(event) {
+	$('#contenedor').load('TutorialWebofScience.html');
+});
+
+
+
+
+$('#link-EmeraldT').click(function(event) {
+	$('#contenedor').load('TutorialEmerald.html');
+});
+
+$('#link-ACMT').click(function(event) {
+	$('#contenedor').load('TutorialACM.html');
+});
+
+$('#link-EbscoT').click(function(event) {
+	$('#contenedor').load('TutorialEbsco.html');
+});
+
+$('#link-EmbaseT').click(function(event) {
+	$('#contenedor').load('TutorialEmbase.html');
+});
+
+$('#link-EngineeringVillageT').click(function(event) {
+	$('#contenedor').load('TutorialEngineeringVillage.html');
+});
+
+$('#link-GaleT').click(function(event) {
+	$('#contenedor').load('TutorialGale.html');
+});
+
+$('#link-LegisT').click(function(event) {
+	$('#contenedor').load('TutorialLegis.html');
+});
+
+$('#link-MicromedexT').click(function(event) {
+	$('#contenedor').load('TutorialMicromedex.html');
+});
+
+$('#link-NNNConsultT').click(function(event) {
+	$('#contenedor').load('TutorialNNNConsult.html');
+});
+
+$('#link-OvidT').click(function(event) {
+	$('#contenedor').load('TutorialOvid.html');
+});
+
+$('#link-ReaxysT').click(function(event) {
+	$('#contenedor').load('TutorialReaxys.html');
+});
+
+$('#link-ScopusT').click(function(event) {
+	$('#contenedor').load('TutorialScopus.html');
+});
+
+$('#link-ScienceDirectT').click(function(event) {
+	$('#contenedor').load('TutorialScienceDirect.html');
+});
+
+$('#link-WebofScienceT').click(function(event) {
+	$('#contenedor').load('TutorialWebofScience.html');
+});
